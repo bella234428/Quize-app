7d0c8f6147f9e9c65c3c52a0038e914ae426e563
bella234428 <ibseali258@gmail.com>
2025-06-29 07:34:10 +0300

    add questions

---
+++ index.js
@@ -0,0 +1,104 @@
+const questions = [
+    {
+        question: "What causes most earthquakes?",
+        answers: [
+            {text: "Volcanic eruptions", correct: false},
+            {text: "Collapsing buildings", correct: false},
+            {text: "Movement of tectonic plates", correct: true},
+            {text: "Strong winds", correct: false},
+        ]
+    },
+
+    {
+        question: "What is the point on the Earth's surface directly above the earthquake origin called?",
+        answers: [
+            {text: "Seismic zone", correct: false},
+            {text: "Hypocenter", correct: false},
+            {text: "Fault line", correct: false},
+            {text: "Epicenter", correct: true},
+        ]
+    },
+
+    {
+        question: "Which scale is commonly used to measure earthquake magnitude?",
+        answers: [
+            {text: " Celsius scale", correct: false},
+            {text: "Beaufort scale", correct: false},
+            {text: "Richter scale ", correct: true},
+            {text: "Fujita scale", correct: false},
+        ]
+    },
+    {
+
+        question: "What instrument is used to detect and record earthquakes?",
+        answers: [
+            {text: "Thermometer", correct: false},
+            {text: "Seismograph", correct: true},
+            {text: "Barometer", correct: false},
+            {text: "Hydrometer", correct: false},
+        ]
+    },
+    {
+         question: "Where do most earthquakes occur?",
+        answers: [
+            {text: "Middle of continents", correct: false},
+            {text: "Along plate boundaries", correct: true},
+            {text: "In deserts", correct: false},
+            {text: "At the equator", correct: false},
+        ]
+    },
+
+    {
+        question: "What is a tsunami most commonly caused by?",
+        answers: [
+            {text: "Hurricanes", correct: false},
+            {text: "Underwater earthquakes", correct: true},
+            {text: "Tidal waves", correct: false},
+            {text: "Icebergs melting", correct: false},
+        ]
+    },
+
+    {
+        question: " Which layer of the Earth do earthquakes typically originate in?",
+        answers: [
+            {text: "Inner core", correct: false},
+            {text: "Outer core", correct: false},
+            {text: " Crust ", correct: true},
+            {text: "Atmosphere", correct: false},
+        ]
+    },
+
+    {
+
+        question: "What does the Richter scale measure?",
+        answers: [
+            {text: "Depth of the earthquake", correct: false},
+            {text: "Speed of the wave", correct: false},
+            {text: " Damage caused", correct: false},
+            {text: "Energy released (magnitude)", correct: true},
+        ]
+    },
+
+    {
+
+        question: "What is the term for smaller quakes that follow a major earthquake?",
+        answers: [
+            {text: " Pre-shocks", correct: false},
+            {text: "Miniquakes", correct: false},
+            {text: "Aftershocks", correct: true},
+            {text: "Foreshocks", correct: false},
+        ]
+    },
+
+    {
+
+        question: "Which country experiences the most earthquakes annually?",
+        answers: [
+            {text: "Brazil", correct: false},
+            {text: " Canada", correct: false},
+            {text: "Japan ", correct: true},
+            {text: "South Africa", correct: false},
+        ]
+    }
+
+]
